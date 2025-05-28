bd0758f652ef331fbe7418b6e0ac56fe2f528a47
OtaShoya <shoya.toawork@gmail.com>
2025-05-28 23:41:39 +0900

    deploy 2025-05-28

--- src/types/firebaseui-react.d.ts
+++ src/types/firebaseui-react.d.ts
@@ -1,9 +1,12 @@
+import { Auth } from "firebase/auth";
+import * as firebaseui from "firebaseui";
+
 declare module "firebaseui-react" {
   import * as React from "react";
 
   interface StyledFirebaseAuthProps {
-    uiConfig: any;
-    auth: any;
+    uiConfig: firebaseui.auth.Config;
+    auth: Auth;
   }
 
   const StyledFirebaseAuth: React.FC<StyledFirebaseAuthProps>;
